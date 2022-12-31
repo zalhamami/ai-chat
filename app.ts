d434447d94b001f2de19e87cff7af6525a9f7af5
hanvee <hanvee@gmail.com>
2022-12-31 16:54:09 +0700

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-
-import Robo from './src';
-import dotenv from 'dotenv';
-dotenv.config();
-import UnsplashService from './src/services/unsplash';
-
-const res = UnsplashService.getPhoto('office');
